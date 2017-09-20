bd77bbaf011466401334177674ff63e015e864f9
faustoTapia <faustoTapia@users.noreply.github.com>
2017-09-21 00:01:44 +1000

    Collision check of linear multidimentional path

--- Assignment-2/src/solution/collisionDetect.java
+++ Assignment-2/src/solution/collisionDetect.java
@@ -2,13 +2,33 @@ package solution;
 
 import java.util.List;
 
+import problem.ASVConfig;
 import problem.Obstacle;
-import tester.*;
+import tester.Tester;
 
 public class collisionDetect {
-	double resolution=0.001;
-	public static boolean isValidPath(Edge path,List<Obstacle> obstacles) {
-		
+	static double resolution=0.001;
+	static Tester test=new Tester();
+	public static boolean isValidPath(ASVConfig getConfigCoords) {	
 		return true;
 	}
+	
+	/**
+	 * Checks whether a path is collision free depending on resolution
+	 * @param edge edge to be checked
+	 * @param obstacles workspace obstacles
+	 * @return boolean
+	 */
+	public static boolean isPathColliding(Edge edge,List<Obstacle> obstacles) {
+		Node middle=edge.getInit().middleNode(edge.getEnd());
+		if (test.hasCollision(middle.getConfigCoords(), obstacles)) {
+			return true;
+		}
+		if (edge.getInit().getLocation().distance(middle.getLocation())<resolution) {
+			return true;
+		}
+		return (isPathColliding (new Edge(edge.getInit(),middle),obstacles)
+				&&isPathColliding (new Edge(middle,edge.getInit()),obstacles));
+	}
+	
 }
