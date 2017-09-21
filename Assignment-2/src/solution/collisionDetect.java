94fe5e62a642db72bc233583bdc59ca9462404ba
faustoTapia <faustoTapia@users.noreply.github.com>
2017-09-21 17:20:00 +1000

    Added node checking functions

--- Assignment-2/src/solution/collisionDetect.java
+++ Assignment-2/src/solution/collisionDetect.java
@@ -9,29 +9,54 @@ import tester.Tester;
 public class collisionDetect {
 	static double resolution=0.001;
 	static Tester test=new Tester();
+	static double angularMaxError=1e5;
 	
-	//PLEASE IMPLEMENT THIS METHOD CORRECTLY
+	/**
+	 * Returns whether a node is Valid
+	 * @param node
+	 * @param obstacles
+	 * @return boolean
+	 */
 	public static boolean isNodeValid(Node node,List<Obstacle> obstacles) {	
+		if (!isConvex(node))
+			return false;
+		if (!test.fitsBounds(node.getConfigCoords()))
+			return false;
+		if (!test.hasEnoughArea(node.getConfigCoords()))
+			return false;
+		if (test.hasCollision(node.getConfigCoords(), obstacles))
+			return false;
+		return true;
+	}
+	/**
+	 * Checks if Node is convex
+	 * @param node
+	 * @return boolean
+	 */
+	public static boolean isConvex(Node node) {
+		double dummyAngle=0;
+		for (int i=1;i<node.getTheta().size();i++) {
+			double readAngle=node.getTheta().get(i);
+			if (readAngle<180+angularMaxError)
+				readAngle=readAngle-360;
+			if (dummyAngle*readAngle<-angularMaxError)
+				return false;
+			if (dummyAngle*readAngle>angularMaxError || dummyAngle*readAngle<-angularMaxError)
+				dummyAngle=readAngle;
+		}
 		return true;
 	}
 	
-	//PLEASE IMPLEMENT THIS METHOD PROPERLY
-	public static Edge nearestEdge(Edge edge,List<Obstacle> obstacles) {
-		return (new Edge(new Node(),new Node()));
-	}
-	
 	/**
-	 * Checks whether a path is collision free depending on resolution
+	 * Checks whether a path is collision free and valid depending on resolution
 	 * @param edge edge to be checked
 	 * @param obstacles workspace obstacles
 	 * @return boolean
 	 */
-	
-	
 	public static boolean isEdgeValid(Edge edge,List<Obstacle> obstacles) {
 		Node middle=edge.middleNode();
-		if (isNodeValid(middle, obstacles)) {
-			return true;
+		if (!isNodeValid(middle, obstacles)) {
+			return false;
 		}
 		if (edge.getInit().getLocation().distance(middle.getLocation())<resolution) {
 			return true;
@@ -40,4 +65,9 @@ public class collisionDetect {
 				&&isEdgeValid (new Edge(middle,edge.getInit()),obstacles));
 	}
 	
+	//PLEASE IMPLEMENT THIS METHOD PROPERLY
+	public static Edge nearestEdge(Edge edge,List<Obstacle> obstacles) {
+		
+		return (new Edge(new Node(),new Node()));
+	}
 }
