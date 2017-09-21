ad8dee57b0fc565da3c91681d645a7c01cbca9f2
faustoTapia <faustoTapia@users.noreply.github.com>
2017-09-21 18:43:23 +1000

    Little change added in PathFinder

--- Assignment-2/src/solution/PathFinder.java
+++ Assignment-2/src/solution/PathFinder.java
@@ -7,9 +7,11 @@ public class PathFinder {
 	private Node goalNode;
 	private Queue<Node> theQueue;
 	private List<Node> inTree;
-	private int k=100;
-	private int maxIter=5000;
-	private int mode=1;
+	private int maxRandWalk = 100;
+	private int maxIter = 5000;
+	private int mode = 1;
+	private int branchingFact=4;
+	
 	public PathFinder(Node init,Node end) {
 		initNode=init;
 		goalNode=end;
